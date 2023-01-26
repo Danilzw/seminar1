a756790762840f43fcdd365bab61005ba79d720e
Danil <shwedx@Yandex.ru>
2023-01-26 16:06:32 +0300

    Random add

--- Example/Example3/Program.cs
+++ Example/Example3/Program.cs
@@ -1,5 +1,7 @@
-﻿double numberA = 12;
-double numberB = 5;
-double result = numberA / numberB;
+﻿int numberA = new Random().Next(1, 10); //1 - 10
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+double result = numberA + numberB;
 Console.WriteLine(result);
 
