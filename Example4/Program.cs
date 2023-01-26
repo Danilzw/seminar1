7ef73613d193471f65e1eb20ae93bc20df220edd
Danil <shwedx@Yandex.ru>
2023-01-26 16:27:24 +0300

    Example4

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Danil")
+{
+    Console.WriteLine("Privet Danil!123");
+}
+else
+{
+    Console.Write("Privet ");
+    Console.WriteLine(username);
+}
